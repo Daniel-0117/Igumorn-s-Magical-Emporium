7870ac930e571efca7f258b92da8f9c578415c04
Daniel-0117 <nevaslon0117@gmail.com>
2025-02-27 17:45:04 -0700

    finished draft of homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import homePage from '../components/homePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const Home = () => {
     return (
