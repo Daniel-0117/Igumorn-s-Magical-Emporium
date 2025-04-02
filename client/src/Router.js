90979e5ddec20ddde8c47219c15b883278d27ca6
Daniel-0117 <nevaslon0117@gmail.com>
2025-04-02 14:03:39 -0700

    added a layout component that displays nav on all pages

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./components/homePage";
 import MagicMerchPage from "./components/magicMerchPage";
+import Layout from "./components/layout/layout"; // adjusted import path to match file casing
 import "./App.css";
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/magic-merch" element={<MagicMerchPage />} />
-                <Route path="*" element={<div>404 - You have fallen into the abyss fool</div>} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/magic-merch" element={<MagicMerchPage />} />
+          <Route path="*" element={<div>404 - You have fallen into the abyss fool</div>} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 };
 
 export default AppRouter;
---
+++ client/src/components/layout/layout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="/">The Magical Store</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/magic-merch">Merchandise</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="layout-container">
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
