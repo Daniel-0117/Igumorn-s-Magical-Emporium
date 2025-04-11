4fbdcd0c0af6e754501c7c5564e9d21f3f49c792
Daniel-0117 <nevaslon0117@gmail.com>
2025-04-11 08:31:12 -0700

    began familiar page

---
+++ client/src/components/familiarPage/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./familiar.css";
+
+const FamiliarsPage = () => {
+  const [Companions, setCompanions] = useState([]);
+  console.log("FamiliarPage Loaded");
+
+  useEffect(() => {
+    fetch("/api/familiars")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Familiar:", data); 
+        setCompanions(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  // Check if magicItems have loaded in
+  if (Companions.length === 0) {
+    return <div>Loading magical friends...</div>;
+  }
+
+  return (
+    <div
+      className="familiar-background-image"
+      style={{
+        backgroundImage: `url(${require("../../assets/Familiar_adoption.jpg")})`,
+      }}
+    >
+
+    <div className="container mt-5">
+      <div className="row">
+        {Companions.map((item) => (
+          <div key={item.id} className="col-md-4 mb-4">
+            <div className="card h-100 shadow-sm" style={{ width: "18rem" }}>
+              <img
+                src={item.image}
+                className="card-img-top"
+                alt={item.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <p className="card-text">{item.description}</p>
+                <p><strong>Price:</strong> {item.price} gold</p>
+                <p><strong>Requirements:</strong> {item.requirements}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default FamiliarsPage;
+
--- server/routes/api.js
+++ server/routes/api.js
@@ -9,11 +9,18 @@ router.get('/test', (req, res) => {
 
 // Load seed data
 const magicMerch = require('../seeds/magicMerchData.json');
+const familiars = require('../seeds/familiarData.json');
 
 // Route to get all magic merchandise
 router.get('/magicMerch', (req, res) => {
   console.log('🔥 /api/magicMerch hit!');
   res.json(magicMerch);
+});
+
+// Route to get all familiars
+router.get('/familiars', (req, res) => {
+  console.log('🐾 /api/familiar hit!');
+  res.json(familiars);
 });
 
 module.exports = router;
