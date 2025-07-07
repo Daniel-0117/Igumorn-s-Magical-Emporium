a5fa69817f76cb7856550bd8bf126d1a23dd950c
Daniel-0117 <nevaslon0117@gmail.com>
2025-07-07 12:19:45 -0700

    tweaked connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/IGISMagicDB
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('✅ MongoDB connected successfully'))
-.catch((err) => console.error('❌ MongoDB connection failed fool:', err.message));  // Log the actual error
+.then(() => console.log('MongoDB connected successfully'))
+.catch((err) => console.error('MongoDB connection failed fool:', err.message));  // Log the actual error
 
 module.exports = mongoose.connection;
