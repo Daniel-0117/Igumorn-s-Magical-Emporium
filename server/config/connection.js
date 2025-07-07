14f08fe0b05957bf0283936502d62e81729f9766
Daniel-0117 <nevaslon0117@gmail.com>
2025-07-07 09:25:43 -0700

    fixed connection issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,8 @@ console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/IGISMagicDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
+.then (() => console.log('MongoDB connected successfully'))
+.catch (() => console.log('MongoDB connection failed fool'));
 
 module.exports = mongoose.connection;
