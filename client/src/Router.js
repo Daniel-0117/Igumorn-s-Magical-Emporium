ff7e535026b94798d7bb4fa2bbf2e1c12ab48bc3
Daniel-0117 <nevaslon0117@gmail.com>
2025-07-07 11:46:31 -0700

    made changes to connection.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./components/homePage";
-import MagicMerchPage from "./components/magicMerchPage";
+import MagicMerchPage from "./pages/MagicalMerch";
 import Login from "./components/loginPage";
-import Layout from "./components/layout/layout"; // adjusted import path to match file casing
-import "./App.css";
+import Layout from "./components/layout/layout";
 import FamiliarsPage from "./components/familiarPage";
+import "./App.css";
 
 const AppRouter = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/magic-merch" element={<MagicMerchPage />} />
-          <Route path="/familiars" element={<FamiliarsPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<div>404 - You have fallen into the abyss fool</div>} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="magic-merch" element={<MagicMerchPage />} />
+          <Route path="familiars" element={<FamiliarsPage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<div style={{ color: "white" }}>404 - You have fallen into the abyss fool</div>} />
+        </Route>
+      </Routes>
     </Router>
   );
 };
--- client/src/components/layout/layout.js
+++ client/src/components/layout/layout.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom"; // import Link
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./layout.css"; // Import your CSS file for layout styles
+import "./layout.css";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container">
-          <a className="navbar-brand" href="/">The Magical Store</a>
+          <Link className="navbar-brand" to="/">The Magical Store</Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -23,20 +24,21 @@ const Layout = ({ children }) => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link" href="/login">Login</a>
+                <Link className="nav-link" to="/login">Login</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/magic-merch">Merchandise</a>
+                <Link className="nav-link" to="/magic-merch">Merchandise</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="familiars">Familiars</a>
+                <Link className="nav-link" to="/familiars">Familiars</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
       <div className="layout-container">
-        {children}
+        {/* This part will be replaced by active route */}
+        <Outlet />
       </div>
     </>
   );
